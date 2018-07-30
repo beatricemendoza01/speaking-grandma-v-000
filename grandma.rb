99341f84d4b8cb5a69664d1fa35aa226224fc827
Beatrice Mendoza <mendoza@nyu.edu>
2018-07-30 21:15:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase.upcase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.capitalize == "Hi Nana, how are you?"
-    "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase == "WHAT DID YOU EAT TODAY?" || phrase.upcase == "WHAT?"
     "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
